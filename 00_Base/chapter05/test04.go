e431cb1e4c6be60b1de79e902102b98099cb4675
wanglg007 <wanglg007@gmail.com>
2019-03-10 19:45:51 +0800

    数组和切片

---
+++ 00_Base/chapter05/test04.go
@@ -0,0 +1,21 @@
+package main
+
+//(5.4)for-range
+import (
+	"fmt"
+)
+
+func main() {
+
+	//演示for-range遍历数组
+	heroes := [...]string{"宋江", "吴用", "卢俊义"}
+
+	for i, v := range heroes {
+		fmt.Printf("i=%v v=%v\n", i, v)
+		fmt.Printf("heroes[%d]=%v\n", i, heroes[i])
+	}
+
+	for _, v := range heroes {
+		fmt.Printf("元素的值=%v\n", v)
+	}
+}
