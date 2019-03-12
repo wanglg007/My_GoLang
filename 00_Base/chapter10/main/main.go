daf29c73eeaf07af7528184fb614d9bf205538a3
wanglg007 <wanglg007@gmail.com>
2019-03-12 21:22:42 +0800

    案例-家庭收支案例

---
+++ 00_Base/chapter10/main/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"github.com/wanglg007/My_GoLang/00_Base/chapter10/utils"
+	"fmt"
+)
+func main() {
+
+	fmt.Println("这个是面向对象的方式完成~~")
+	utils.NewFamilyAccount().MainMenu() //思路非常清晰
+}
