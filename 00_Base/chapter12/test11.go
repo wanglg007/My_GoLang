3ac6e2bc97fcae7c14bb8ac28c1b07ee9ea9017a
wanglg007 <wanglg007@gmail.com>
2019-03-13 20:43:16 +0800

    文件操作

---
+++ 00_Base/chapter12/test11.go
@@ -0,0 +1,16 @@
+package main
+
+//(12.11)命令行参数
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("命令行的参数有", len(os.Args))
+	//遍历os.Args切片，就可以得到所有的命令行输入参数值
+	for i, v := range os.Args {
+		fmt.Printf("args[%v]=%v\n", i, v)
+	}
+}
