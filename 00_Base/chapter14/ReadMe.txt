5efc1619d8236c5a578b2df2ab6818dd0a520fed
wanglg007 <wanglg007@gmail.com>
2019-03-13 21:50:29 +0800

    goroutine和channel

---
+++ 00_Base/chapter14/ReadMe.txt
@@ -0,0 +1,1 @@
+#goroutine和channel
---
+++ 00_Base/chapter14/test01.go
@@ -0,0 +1,16 @@
+package main
+
+//(14.1)设置Golang运行的CPU数
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	cpuNum := runtime.NumCPU()
+	fmt.Println("cpuNum=", cpuNum)
+
+	//可以自己设置使用多个cpu
+	runtime.GOMAXPROCS(cpuNum - 1)
+	fmt.Println("ok")
+}
