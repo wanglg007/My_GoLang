509fdcef0d773c035b6fec44f2f93527ab5edb5c
wanglg007 <wanglg007@gmail.com>
2019-03-13 22:09:44 +0800

    反射

---
+++ 00_Base/chapter15/test01.go
@@ -0,0 +1,14 @@
+package main
+
+//(15.1)修改变量的值
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var str string = "tom"      //ok
+	fs := reflect.ValueOf(&str) //ok fs -> string
+	fs.Elem().SetString("jack") //ok
+	fmt.Printf("%v\n", str)     // jack
+}
